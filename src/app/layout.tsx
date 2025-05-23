fcedb1f6777eee78fdda1cee2c9a16bfc4aca218
0xjaywins <jaytech639@gmail.com>
2025-05-23 14:22:06 +0100

    updates stake integrations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
-// import "./globals.css";
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <FrameProvider>{children}</FrameProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter, Orbitron } from "next/font/google";
 import "./globals.css";
-// import { AppStateProvider } from "../../components/navigation/navigation-context";
 import { FrameProvider } from "../../components/providers/farcaster-provider";
+import farcasterFrame from "@farcaster/frame-wagmi-connector";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -30,6 +18,17 @@ const orbitron = Orbitron({
 export const metadata: Metadata = {
   title: "NEXUS | Web3 DeFi on Monad",
   description: "Swap, Stake, and Stay updated effortlessly on Monad Testnet",
+  openGraph: {
+    title: "NEXUS | Web3 DeFi on Monad",
+    description: "Swap, Stake, and Stay updated effortlessly on Monad Testnet",
+    images: [
+      {
+        url: "https:https://nexus-ten-sigma.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -39,11 +38,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="fc:frame" content={JSON.stringify(farcasterFrame)} />
+      </head>
       <body
         className={`${inter.variable} ${orbitron.variable} font-sans bg-void dark`}
       >
         <FrameProvider>
-          <div className="min-h-screen bg-void overflow-hidden relative">
+          <div className="min-h-screen bg-[url('/og-image.png')] bg-cover bg-center bg-no-repeat overflow-hidden relative">
             {children}
           </div>
         </FrameProvider>
